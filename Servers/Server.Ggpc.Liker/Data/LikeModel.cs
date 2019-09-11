a6560a78f6b9d22c18c9eddef819f98bf1a5d0b2
arman <arman_sp98@yahoo.com>
2019-09-11 18:22:48 +0430

    littel fix Like server

--- Servers/Server.Ggpc.Liker/Data/LikeModel.cs
+++ Servers/Server.Ggpc.Liker/Data/LikeModel.cs
@@ -1,9 +1,9 @@
-﻿using Entities;
-
+﻿
 namespace Server.Ggpc.Liker.Data
 {
-    public class LikeModel:BaseEntity
+    public class LikeModel
     {
+        public int Id { get; set; }
         public int PostId { get; set; }
         public string UserId { get; set; }
         public bool IsLiked { get; set; }

ca49cb2bc687c0894a2deef72c141016d63b0ae7
Codehaks <ghods.hakim@gmail.com>
2019-09-25 05:36:38 +0330

    Email Sender

---
+++ Web/Portal/Infrastructure/EmailSender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+namespace App.Services
+{
+  
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            
+
+            return Task.CompletedTask;
+        }
+    }
+}
